d01750b24b73fee4265d49616391e4eb2da17394
wardxela <axelwardweb@gmail.com>
2022-09-23 18:56:38 +0300

    fix(5.py): reorder if statements to work correctly

--- 5.py
+++ 5.py
@@ -1,24 +1,24 @@
 from random import randint
 
-print('Угадай число от 1 до 1000')
+print('Угадай число от 1 до 1000\n')
 num_to_guess = randint(1, 1000)
 total_attempts = left_attempts = 10
 won = False
 while left_attempts > 0:
     left_attempts -= 1
-    attempt = int(input('Ваше ответ:'))
+    attempt = int(input('Ваше ответ: '))
     if num_to_guess == attempt:
         won = True
         break
-    if num_to_guess * 0.7 < attempt and num_to_guess * 1.3 > attempt:
-        print('Теплее')
-    elif num_to_guess * 0.9 < attempt and num_to_guess * 1.1 > attempt:
+    if num_to_guess * 0.9 < attempt and num_to_guess * 1.1 > attempt:
         print('Горячо')
+    elif num_to_guess * 0.7 < attempt and num_to_guess * 1.3 > attempt:
+        print('Теплее')
     else:
         print('Холодно')
     print('Попробуй число ', end='')
     print('побольше' if num_to_guess > attempt else 'поменьше')
-    print(f'Осталось попыток: {left_attempts}')
+    print(f'Осталось попыток: {left_attempts}\n')
 if won:
     print(
         f'Число угадано за {total_attempts - left_attempts} попыток. Поздравляю!')
