0ba62366c1d985a1031815f59a537616b39af32d
wardxela <axelwardweb@gmail.com>
2022-10-27 17:33:49 +0300

    refactor

--- sum.py
+++ sum.py
@@ -1,31 +1,24 @@
-# Программа основана на идее состояний
-# Состояние 0 - начальное состояние
-# Состояние 1 - состояние накопления суммы
-# Состояние 2 - конец программы
+from random import randint
 
 acc = 0
-state = 0
-counter = 1
+shouldSum = False
+current = randint(0, 10000)
 
 print('Доступные команды: start, end')
 while True:
-    if state == 0:
-        command = input('Введите команду start ')
-        if command == 'start':
-            state = 1
-        else:
-            print('Неверный ввод. Я жду команду start')
-    elif state == 1:
-        numOrEnd = input(f'{counter}: ')
-        if numOrEnd == 'end':
-            state = 2
-        else:
-            try:
-                num = int(numOrEnd)
-                counter += 1
-                acc += num
-            except:
-                print('Неверный ввод. Я жду число')
-    elif state == 2:
-        print(acc)
+    print(f'Текущее число: {current}')
+    command = input('>>> ')
+
+    if shouldSum:
+        acc += current
+        print('Текущая сумма:', acc)
+
+    if command == 'start' and not shouldSum:
+        print('Начинаю считать...')
+        shouldSum = True
+    elif command == 'end':
         break
+
+    current += 1
+
+print('Накопленная сумма:', acc)
